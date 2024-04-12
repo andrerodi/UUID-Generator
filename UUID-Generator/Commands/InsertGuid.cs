0270c92dc255d486e4a180308ca1130a3d2ecad1
andrerodi <89607119+andrerodi@users.noreply.github.com>
2024-04-12 23:14:00 +0200

    Initial commit

---
+++ UUID-Generator/Commands/InsertGuid.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+namespace UUID_Generator
+{
+    [Command(PackageIds.InsertGuid)]
+    internal sealed class InsertGuid : BaseCommand<InsertGuid>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            var docView = await VS.Documents.GetActiveDocumentViewAsync();
+
+            var selection = docView?.TextView.Selection.SelectedSpans.FirstOrDefault();
+
+            if (selection is null)
+            {
+                return;
+            }
+
+            var uuid = Guid.NewGuid().ToString();
+
+            docView.TextBuffer.Replace(selection.Value, uuid);
+        }
+    }
+}
---
+++ UUID-Generator/Commands/InsertUlid.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+namespace UUID_Generator.Commands
+{
+    [Command(PackageIds.InsertUlid)]
+    internal sealed class InsertUlid : BaseCommand<InsertUlid>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            var docView = await VS.Documents.GetActiveDocumentViewAsync();
+
+            var selection = docView?.TextView.Selection.SelectedSpans.FirstOrDefault();
+
+            if (selection is null)
+            {
+                return;
+            }
+
+            var uuid = Ulid.NewUlid().ToGuid().ToString();
+
+            docView.TextBuffer.Replace(selection.Value, uuid);
+        }
+    }
+}
